4062dec17cba90f429490e16a082d3079b76bfce
Florin Ilie <ilieflorin132@gmail.com>
2025-12-23 10:45:52 -0800

    Initial Commit

---
+++ rebu/rebuApp.swift
@@ -0,0 +1,17 @@
+//
+//  rebuApp.swift
+//  rebu
+//
+//  Created by silver on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct rebuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
